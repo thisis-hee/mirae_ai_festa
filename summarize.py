ac6c9868c6bd759bb95a5fc8ef6fe3f25f7a0c1f
gunheee-leee <ish8176@dgu.ac.kr>
2024-07-20 19:54:01 +0900

    summarize.py 추가

---
+++ summarize.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+import pandas as pd
+import csv
+import json
+
+
+# 네이버 클라우드 플랫폼에서 발급받은 클라이언트 아이디와 시크릿
+client_id = "CLIENT_ID"
+client_secret = "CLIENT_SECRET"
+
+# API 엔드포인트 URL
+url = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize"
+
+# 요청 헤더 설정
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/json"
+}
+
+# Content만 빼서 json형태로 바꾸는 함수 지정
+def to_json(csv_path):
+    data_contents=pd.read_csv(csv_path)
+    data_contents=data_contents['Content']
+    data_contents.to_json('Contents.json', orient = 'index', indent = 4)
+
+
+def summarize(json_path):
+    with open(json_path, 'r') as f:
+        json_data = json.load(f)
+
+    summarized_data = []
+
+    for i in range(len(json_data)):
+        index=i
+        specific_row = json_data[str(index)]
+
+        data = {
+            "document": {
+                "content": specific_row
+            },
+            "option": {
+                "language": "ko",
+                "model": "general",
+                "tone": 0,
+                "summaryCount": 3
+            }
+        }
+        # POST 요청 보내기
+        response = requests.post(url, headers=headers, json=data)
+
+        # 응답 처리
+        if response.status_code == 200:
+            summary_result = response.json()
+            summarized_text = summary_result.get("summary", "")  # 요약 결과에서 실제 요약 텍스트 추출 (필요에 따라 수정)
+            summarized_data.append({"Text": summarized_text, "Completion": ''})
+        else:
+            print("요청 실패: 상태 코드", response.status_code)
+            print("응답 내용:", response.text)
+            summarized_data.append({"Text": "요약 실패", "Completion": ""})
+
+    df_summarized = pd.DataFrame(summarized_data)
+    with open('summarizedContents.json', 'w', encoding='utf-8') as f:
+        json.dump(df_summarized.to_dict(orient='records'), f, ensure_ascii=False, indent=4)
+
+
+to_json("data_comments.csv")
+summarize("Contents.json")
